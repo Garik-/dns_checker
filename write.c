c078fbf5458abd38bf3b4a02d7af1c52196fe2f5
Gar|k <garik.djan@gmail.com>
2016-07-26 15:40:41 +0500

    Write DSV format

--- write.c
+++ write.c
@@ -6,17 +6,6 @@
 
 #include "main.h"
 
-#define MAGIC 0xDEADBEEF
-
-#pragma pack(push,1)
-
-typedef struct {
-    unsigned int magic;
-    unsigned int data_len;
-} file_record;
-
-#pragma pack(pop)
-
 static ssize_t /* Write "n" bytes to a descriptor. */
 writen(int fd, const void *vptr, size_t n) {
     size_t nleft;
@@ -47,30 +36,8 @@ write_out(const options_t * options, const struct hostent *host) {
     char buf[MAXLINE];
     int position;
     unsigned int len;
-    if (OUT_DEFAULT == options->file.out) {
-        len = snprintf(buf, sizeof (buf), "%s: %s\n", host->h_name, inet_ntoa(*((struct in_addr *) host->h_addr_list[0])));
-
-    } else {
-        file_record *rec = (file_record *) buf;
-        rec->magic = MAGIC;
-        position = sizeof (file_record);
-
-
-        len = strlen(host->h_name);
-        memcpy(&buf[position], &len, sizeof (len));
-        position += sizeof (len);
-        memcpy(&buf[position], host->h_name, len);
-        position += len;
-
-        memcpy(&buf[position], &host->h_length, sizeof (host->h_length));
-        position += sizeof (host->h_length);
-        memcpy(&buf[position], host->h_addr_list[0], host->h_length);
-        position += host->h_length;
-
-        rec->data_len = position - sizeof (file_record);
-
-        len = position;
-    }
+    
+    len = snprintf(buf, sizeof (buf), "%s:%s\n", host->h_name, inet_ntoa(*((struct in_addr *) host->h_addr_list[0])));
 
     if (len != writen(options->file.out, buf, len)) {
         err_ret("[E] write_out");
